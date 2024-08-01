3d181ac0e9700180fbbf362bbba7d7f4a140ebcd
kelvinkioi <kelvinkioi.101k@gmail.com>
2024-08-01 20:30:48 +0300

    Island Perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -16,7 +16,8 @@ def island_perimeter(grid):
     grid is rectangular, with its width and height not exceeding 100
     The grid is completely surrounded by water
     There is only one island (or nothing).
-    The island doesn’t have “lakes” (water inside that isn’t connected to the water surrounding the island).
+    The island doesn’t have “lakes” (water inside that isn’t
+    connected to the water surrounding the island).
     """
     perimeter = 0
     rows = len(grid)
