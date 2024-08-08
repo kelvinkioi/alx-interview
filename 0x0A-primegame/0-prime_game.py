96e21c619c38afd653de6564acc3279b0a2bcd61
kelvinkioi <kelvinkioi.101k@gmail.com>
2024-08-09 01:51:07 +0300

    prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
 """
-Maria and Ben are playing a game. Given a set of consecutive integers
-starting from 1 up to and including n, they take turns choosing a
-prime number from the set and removing that number and its multiples
-from the set. The player that cannot make a move loses the game.
-They play x rounds of the game, where n may be different for each round.
-Assuming Maria alwaysgoes first and both players play optimally,
-determine who the winner of each game is.
+Prime game
 """
 
 
